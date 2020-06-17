eeeae765edb4a4a237c0e18c7b0b39e70ef80b20
Ulyana Ermoshina <uermoshina@inbox.ru>
2020-06-17 20:55:54 +0300

    In commit

--- 3.4_dependency/cart/src/test/java/ru/netology/manager/CartManagerTestNonEmpty.java
+++ 3.4_dependency/cart/src/test/java/ru/netology/manager/CartManagerTestNonEmpty.java
@@ -1,27 +1,26 @@
 package ru.netology.manager;
 
 import org.junit.jupiter.api.Test;
-import ru.netology.domain.PurchaseItem;
-import ru.netology.manager.CartManager;
+import ru.netology.domain.Movies;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CartManagerTestNonEmpty {
   @Test
   public void shouldRemoveIfExists() {
-    CartManager manager = new CartManager();
+    AfishaManager manager = new AfishaManager(5);
     int idToRemove = 1;
-    PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
-    PurchaseItem second = new PurchaseItem(2, 2, "second", 1, 1);
-    PurchaseItem third = new PurchaseItem(3, 3, "third", 1, 1);
+    Movies first = new Movies(1, 1, "first", 1, 1);
+    Movies second = new Movies(2, 2, "second", 1, 1);
+    Movies third = new Movies(3, 3, "third", 1, 1);
     manager.add(first);
     manager.add(second);
     manager.add(third);
 
     manager.removeById(idToRemove);
 
-    PurchaseItem[] actual = manager.getAll();
-    PurchaseItem[] expected = new PurchaseItem[]{third, second};
+    Movies[] actual = manager.getAll();
+    Movies[] expected = new Movies[]{third, second};
 
 //    assertEquals(expected, actual);
     assertArrayEquals(expected, actual);
@@ -29,19 +28,19 @@ public class CartManagerTestNonEmpty {
 
   @Test
   public void shouldNotRemoveIfNotExists() {
-    CartManager manager = new CartManager();
+    AfishaManager manager = new AfishaManager();
     int idToRemove = 4;
-    PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
-    PurchaseItem second = new PurchaseItem(2, 2, "second", 1, 1);
-    PurchaseItem third = new PurchaseItem(3, 3, "third", 1, 1);
+    Movies first = new Movies(1, 1, "first", 1, 1);
+    Movies second = new Movies(2, 2, "second", 1, 1);
+    Movies third = new Movies(3, 3, "third", 1, 1);
     manager.add(first);
     manager.add(second);
     manager.add(third);
 
     manager.removeById(idToRemove);
 
-    PurchaseItem[] actual = manager.getAll();
-    PurchaseItem[] expected = new PurchaseItem[]{third, second, first};
+    Movies[] actual = manager.getAll();
+    Movies[] expected = new Movies[]{third, second, first};
 
     assertArrayEquals(expected, actual);
   }
